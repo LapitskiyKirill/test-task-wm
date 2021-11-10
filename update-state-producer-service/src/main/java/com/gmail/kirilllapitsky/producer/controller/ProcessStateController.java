3392e6d6696275e547e28e7a400358f73cba3c7f
krilllapitsky <krilllapitsky@gmail.com>
2021-11-10 11:26:12 +0300

    add swagger ui

--- update-state-producer-service/src/main/java/com/gmail/kirilllapitsky/producer/controller/ProcessStateController.java
+++ update-state-producer-service/src/main/java/com/gmail/kirilllapitsky/producer/controller/ProcessStateController.java
@@ -1,6 +1,10 @@
 package com.gmail.kirilllapitsky.producer.controller;
 
 import com.gmail.kirilllapitsky.producer.service.KafkaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,8 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProcessStateController {
     private final KafkaService kafkaService;
 
+    @Operation(summary = "Set next state to employee with specified id in parameter.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Employee state successfully updated."),
+            @ApiResponse(responseCode = "500", description = "Some problems while processing state.")})
     @PostMapping("/{employeeId}")
     public void processEmployeeState(
+            @Parameter(required = true, description = "Id of employee that needs to set next state.")
             @PathVariable Long employeeId) {
         kafkaService.sendMessage(employeeId);
     }
